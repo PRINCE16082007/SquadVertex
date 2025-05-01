b8d149b2a6867f4d863ac15c01c509df1054f467
prince16082007 <princegahlot2007@gmail.com>
2025-05-01 15:12:15 +0530

    Create sw.js

---
+++ schedule_tracker/boys_schedule_tracker/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'sv-tracker-v1';
+const URLS = [
+  './boys_schedule_tracker.html',
+  './site.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  // add your CSS/JS assets here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
