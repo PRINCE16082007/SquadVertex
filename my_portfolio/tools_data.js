40075f581939b1893cd1a0cb50d3b81c5f2c63e9
prince16082007 <princegahlot2007@gmail.com>
2026-01-06 16:38:24 +0530

    Update tools_data.js

--- my_portfolio/tools_data.js
+++ my_portfolio/tools_data.js
@@ -1,32 +1,29 @@
 const toolsData = [
     {
         "id": 1,
-        // Yahan apna Dropbox Image Link daalna (?raw=1 ke saath)
         "image": "https://dl.dropboxusercontent.com/scl/fi/51zrnrjjg6ofwddf9k4vw/IMG_20260106_132222.jpg?rlkey=m4p7wr0anr6abl6mjitjdkwxx&st=nwu9ix1g&dl=0", 
         "title": "GitHub Repo Analyzer",
         "description": "Get deep insights into any GitHub repository. Visual stats, spaghetti code meter, and health checks for developers.",
         "stars": 5,
         "tag": "For Developers",
-        "link": "repo_analyzer.html" 
+        "link": "/saas_tools/repo_analyzer.html" 
     },
     {
         "id": 2,
-        // Yahan apna Dropbox Image Link daalna
         "image": "https://dl.dropboxusercontent.com/scl/fi/m3qsrfd8p2kzawymdqyho/IMG_20260106_132256.jpg?rlkey=zpwwrj6rqrf54v5fpcoe9b95k&st=vgpou2mt&dl=0",
         "title": "Quick PDF Editor",
         "description": "Securely merge, split, and organize PDF files directly in your browser. No server uploads, 100% private.",
         "stars": 4.5,
         "tag": "Productivity",
-        "link": "pdf_editor.html"
+        "link": "/saas_tools/pdf_editor.html"
     },
     {
         "id": 3,
-        // Yahan apna Dropbox Image Link daalna
         "image": "https://dl.dropboxusercontent.com/scl/fi/kzfxa7mxf2u5okehpx2ex/IMG_20260106_132315.jpg?rlkey=x1qdteasue9hafe23jjt7di0p&st=pall8o53&dl=0",
         "title": "Font Size Visualizer",
         "description": "Instantly preview typography scales and font sizes. Essential for UI/UX designers to fix spacing and readability.",
         "stars": 4,
         "tag": "Design Tool",
-        "link": "font_size_visualizer.html"
+        "link": "/saas_tools/font_size_visualizer.html"
     }
 ];
